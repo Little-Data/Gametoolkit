46268a602f31bf75bc9ecb366820f02d9b8ef425
Little-data <53138165+Little-Data@users.noreply.github.com>
2026-05-18 23:59:41 +0800

    2359

--- src/data/authors.js
+++ src/data/authors.js
@@ -4,7 +4,7 @@ export const authors = {
     name: "半个水果",
     title: "Little-data",
     url: "https://space.bilibili.com/357695126",
-    image_url: "/Gametoolkit/img/ico.jpg",
+    image_url: "/img/ico.jpg",
     bio: "网站创建者/文档维护者",
     social: {
         bilibili:'https://space.bilibili.com/357695126',
@@ -14,14 +14,14 @@ export const authors = {
   未知作者: {
     name: "未知作者",
     title: "还无法知晓作者",
-    image_url: "/Gametoolkit/img/Unknow_authors.jpg",
+    image_url: "/img/Unknow_authors.jpg",
   },
   偷吃布丁的涅普缇努 : {
     name: "偷吃布丁的涅普缇努",
     title: "喜欢汉化软件、打游戏、听歌",
     url: "https://space.bilibili.com/210298091",
     bio: "这里的作者信息不会自动更新",
-    image_url: "/Gametoolkit/img/8f62fd92dce302a61f8d27fb253f8ae3b3e8f8c7.jpg",
+    image_url: "/img/8f62fd92dce302a61f8d27fb253f8ae3b3e8f8c7.jpg",
     social: {
         bilibili: 'https://space.bilibili.com/210298091',
         github: 'https://github.com/595554963github'
@@ -30,34 +30,34 @@ export const authors = {
   Pinocchio: {
   name: "Pinocchio",
   title: "整理",
-  image_url: "/Gametoolkit/img/Unknow_authors.jpg",
+  image_url: "/img/Unknow_authors.jpg",
   },
   pinokio: {
   name: "pinokio@163.com",
   title: "翻译",
-  image_url: "/Gametoolkit/img/Unknow_authors.jpg",
+  image_url: "/img/Unknow_authors.jpg",
   },
   YZB: {
   name: "YZB",
   title: "文章编者",
-  image_url: "/Gametoolkit/img/Unknow_authors.jpg",
+  image_url: "/img/Unknow_authors.jpg",
   },
   施柯昱: {
   name: "施柯昱",
   title: "作者",
-  image_url: "/Gametoolkit/img/Unknow_authors.jpg",
+  image_url: "/img/Unknow_authors.jpg",
   },
   梁文豪: {
   name: "梁文豪",
   title: "电子书制作",
-  image_url: "/Gametoolkit/img/Unknow_authors.jpg",
+  image_url: "/img/Unknow_authors.jpg",
   },
   中国宋庄: {
     name: "中国宋庄",
     title: "老男人一个。",
     url: "https://space.bilibili.com/247898473",
     bio: "这里的作者信息不会自动更新",
-    image_url: "/Gametoolkit/img/ac03e52373a1ab791dfb5fed1b679d032f0721f0.jpg",
+    image_url: "/img/ac03e52373a1ab791dfb5fed1b679d032f0721f0.jpg",
     social: {
         bilibili: 'https://space.bilibili.com/247898473',
     },
